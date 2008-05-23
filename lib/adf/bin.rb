fbdd765ed271b80d82160537e019d0b108f276d0
remi <remi@remitaylor.com>
2008-05-23 00:14:41 -0700

    added the PDF ADF 1.0 spec as well as 2 DTDs and an XSD and abunchof samples to use for specs

--- lib/adf/bin.rb
+++ lib/adf/bin.rb
@@ -25,16 +25,45 @@ class ADF::Bin
 doco
   end 
 
-  def example_help
+  def validate_help
     <<doco
-Usage: #{ script_name } example
+Usage: #{ script_name } validate adf-sample.xml
 
+  Options:
+    -d, --dtd         Validates using DTD *
+    -a, --altdtd      Validates using alternate DTD *
+    -x, --xsd         Validates using XSD *
+    -o, --objects     Validates using this library's objects
+                      
+                      * requires xmllint
   Summary:
-    Just an example
+    Validates an ADF formatted file
 doco
   end
-  def example *args
-    puts "example: #{ args.inspect }"
+  def validate *args
+    options = { :dtd => false, :altdtd => false, :xsd => false, :objects => false }
+    opts = OptionParser.new do |opts|
+      opts.on('-d','--dtd')     { options[:dtd]     = true }
+      opts.on('-a','--altdtd')  { options[:altdtd]  = true }
+      opts.on('-x','--xsd')     { options[:xsd]     = true }
+      opts.on('-o','--objects') { options[:objects] = true }
+    end
+    opts.parse! args
+    file = args.last
+
+    unless file and File.file? file
+      puts 'You must pass in a file to validate'
+      exit
+    end
+    unless options[:dtd] or options[:altdtd] or options[:xsd] or options[:objects]
+      puts 'Validation type not selected, defaulting to --dtd'
+      options[:dtd] = true
+    end
+
+    puts `xmllint --dtdvalid '#{ File.dirname(__FILE__) + '/../resources/ADF-1.0.dtd' }' #{ file }` if options[:dtd]
+    puts `xmllint --dtdvalid '#{ File.dirname(__FILE__) + '/../resources/ADF-1.0.improved.dtd' }' #{ file }` if options[:altdtd]
+    puts `xmllint --schema '#{ File.dirname(__FILE__)   + '/../resources/ADF-1.0.xsd' }' #{ file }` if options[:xsd]
+    puts '--objects validation not yet supported' if options[:objects]
   end
 
 end
